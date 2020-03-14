3fcf68131819e8c7d3ade6d4fc49e62ce90cd7a8
AlvinChen <44321546+tongjirc@users.noreply.github.com>
2020-03-14 23:43:58 +0800

    03/14 No.300: lengthOfLIS

---
+++ 300.py
@@ -0,0 +1,19 @@
+import collections
+
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        if not nums:
+            return 0
+        dp = []
+        for i in range(len(nums)):
+            dp.append(1)
+            for j in range(i):
+                if nums[i] > nums[j]:
+                    dp[i] = max(dp[i], dp[j] + 1)
+        return max(dp)
+
+
+if __name__=="__main__":
+	s=Solution()
+	nums=eval(input())
+	print(s.lengthOfLIS(nums))
