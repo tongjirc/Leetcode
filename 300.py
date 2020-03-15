6b57acb37c410a7fa60c01b290910c78045d0322
AlvinChen <44321546+tongjirc@users.noreply.github.com>
2020-03-15 16:21:44 +0800

    03/15 No.695: Island space

--- 300.py
+++ 300.py
@@ -1,15 +1,17 @@
 import collections
 
-class Solution:
-    def lengthOfLIS(self, nums: List[int]) -> int:
-        if not nums:
-            return 0
-        dp = []
-        for i in range(len(nums)):
-            dp.append(1)
-            for j in range(i):
-                if nums[i] > nums[j]:
-                    dp[i] = max(dp[i], dp[j] + 1)
+class Solution(object):
+    def lengthOfLIS(self, nums):
+        """
+        :type grid: List[List[int]]
+        :rtype: int
+        """
+        dp=[1 for i in nums]
+        for i in range(0,len(nums)):
+            for j in range(0,i):
+                if nums[j]>nums[i]:
+                    #j: previous stage i: current stage
+                    dp[i]=max(dp[i],dp[j]+1)
         return max(dp)
 
 
