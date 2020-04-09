adc5b01679abd4970f0f3d181ebac1911f81ada9
AlvinChen <44321546+tongjirc@users.noreply.github.com>
2020-04-09 23:45:28 +0800

    04/09 No.22

---
+++ 22.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr  3 00:12:40 2020
+
+@author: AlvinChen
+"""
+class Solution:
+    def generateParenthesis(self, n):
+
+            return num
+
+
+
+if __name__=="__main__":
+    try:
+        a=Solution()
+        n= eval(input())
+        print(a.generateParenthesis(n))
+    except Exception as e:
+        print(e)
