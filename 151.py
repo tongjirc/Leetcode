047aa39c16eee8f8a7b67f14767cc8971bcec822
AlvinChen <44321546+tongjirc@users.noreply.github.com>
2020-04-10 23:48:25 +0800

    04/10 No.151

---
+++ 151.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr 10 16:59:27 2020
+
+@author: AlvinChen
+"""
+
+class Solution:
+    def reverseWords(self, s):
+        slst=s.strip().split(" ")
+        slst.reverse()
+        sclean=[i for i in slst if not (" "  in i or not i)]
+        newstr=" ".join(sclean)
+        return newstr
+
+class Solution:
+    def reverseWords(self, s: str) -> str:
+        return ' '.join(reversed(s.split()))
+
+if __name__=="__main__":
+    try:
+        s=Solution()
+        s=input()
+        print(s.reverseWords(s))
+    except Exception as e:
+        print(e)
