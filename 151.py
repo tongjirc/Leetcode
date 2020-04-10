ac22a98f2e5437b541ca8413d5d85e1c650e1d3c
AlvinChen <44321546+tongjirc@users.noreply.github.com>
2020-04-10 23:54:04 +0800

    Update 151.py

--- 151.py
+++ 151.py
@@ -5,13 +5,13 @@ Created on Fri Apr 10 16:59:27 2020
 @author: AlvinChen
 """
 
-class Solution:
-    def reverseWords(self, s):
-        slst=s.strip().split(" ")
-        slst.reverse()
-        sclean=[i for i in slst if not (" "  in i or not i)]
-        newstr=" ".join(sclean)
-        return newstr
+#class Solution:
+#    def reverseWords(self, s):
+#        slst=s.strip().split(" ")
+#        slst.reverse()
+#        sclean=[i for i in slst if not (" "  in i or not i)]
+#        newstr=" ".join(sclean)
+#        return newstr
 
 class Solution:
     def reverseWords(self, s: str) -> str:
