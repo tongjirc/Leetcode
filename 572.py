e014d716e6bfd31bfe601d25817c72b0940b6ded
AlvinChen <44321546+tongjirc@users.noreply.github.com>
2020-05-08 00:23:30 +0800

    05/08 No.572

--- 572.py
+++ 572.py
@@ -5,14 +5,35 @@ Created on Thu May  7 17:52:00 2020
 @author: AlvinChen
 """
 # Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
-class Solution:
-    def isSubtree(self, s: TreeNode, t: TreeNode) -> bool:
+#class TreeNode:
+#    def __init__(self, val=0, left=None, right=None):
+#        self.val = val
+#        self.left = left
+#        self.right = right
 
+class Solution:
+    def isSubtree(self, s, t):
+        bfsLst=[s]
+        def bfs():
+            while bfsLst:
+                Node=bfsLst.pop(0)
+                if isequal(Node,t):return True
+                if Node.left:bfsLst.append(Node.left)
+                if Node.right:bfsLst.append(Node.right)
+            else:return False
+
+        def isequal(Node1,t):
+            eqlst=[(Node1,t)]
+            while eqlst:
+                now,target=eqlst.pop(0)
+                if now.val!=target.val:return False
+                if  (now.left != 0) ^ (target.left != 0):return False
+                if (now.right != 0) ^ (target.right != 0):return False
+                if now.left: eqlst.append((now.left,target.left))
+                if now.right: eqlst.append((now.right,target.right))
+            else:return True
+
+        return bfs()
 
 if __name__=="__main__":
     try:
