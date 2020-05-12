31adfb0c2f5a72616e048d45a1d0341e8413a90d
AlvinChen <44321546+tongjirc@users.noreply.github.com>
2020-05-12 17:19:18 +0800

    Update 155.py

--- 155.py
+++ 155.py
@@ -10,19 +10,35 @@ class MinStack:
         """
         initialize your data structure here.
         """
-
+        self.lst=[]
+        self.orderlst=[]
+        self.length=0
+        return
 
     def push(self, x: int) -> None:
-
+        if not self.length: self.orderlst.append(x)
+        else:
+            i=self.length-1
+            while i>=0 and self.orderlst[i]<=x:
+                i-=1
+            self.orderlst.insert(i+1,x)
+        self.lst.append(x)
+        self.length+=1
+        return
 
     def pop(self) -> None:
+        if not self.length: return
+        self.orderlst.remove(self.lst.pop())
+        self.length-=1
+        return
 
 
     def top(self) -> int:
+        return self.lst[-1] if self.length else None
 
 
     def getMin(self) -> int:
-
+        return self.orderlst[-1] if self.length else None
 
 
 # Your MinStack object will be instantiated and called as such:
