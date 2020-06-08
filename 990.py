fc1a0edaa2cf89a9f877fbb686f2e739565f6a4d
AlvinChen <44321546+tongjirc@users.noreply.github.com>
2020-06-08 16:01:02 +0800

    0608

---
+++ 990.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 27 08:51:44 2020
+
+@author: AlvinChen
+"""
+class Solution:
+
+    class UnionFind:
+        def __init__(self):
+            self.parent = list(range(26))
+
+        def find(self, index):
+            if index == self.parent[index]:
+                return index
+            self.parent[index] = self.find(self.parent[index])
+            return self.parent[index]
+
+        def union(self, index1, index2):
+            self.parent[self.find(index1)] = self.find(index2)
+
+
+    def equationsPossible(self, equations: List[str]) -> bool:
+        uf = Solution.UnionFind()
+        for st in equations:
+            if st[1] == "=":
+                index1 = ord(st[0]) - ord("a")
+                index2 = ord(st[3]) - ord("a")
+                uf.union(index1, index2)
+        for st in equations:
+            if st[1] == "!":
+                index1 = ord(st[0]) - ord("a")
+                index2 = ord(st[3]) - ord("a")
+                if uf.find(index1) == uf.find(index2):
+                    return False
+        return True
+
+
+
+if __name__=="__main__":
+    try:
+        a=Solution()
+        equations= eval(input())
+        print(a.equationsPossible(equations))
+    except Exception as e:
+        print(e)
