361930e28c511f75b369be662038926b2879e81a
AlvinChen <44321546+tongjirc@users.noreply.github.com>
2020-06-23 12:14:11 +0800

    0623

--- 67.py
+++ 67.py
@@ -4,7 +4,7 @@ Created on Wed Apr  1 23:45:36 2020
 
 @author: AlvinChen
 """
-class Solution:
+class Solution1:
     def addBinary(self, a,b):
         c,d=int(a,2),int(b,2)
         str1=""
@@ -15,6 +15,46 @@ class Solution:
         if e<2:
             str1=str(e)+str1
         return str1
+
+import collections
+class Solution2:
+    def addBinary(self, a,b):
+        l1,l2=len(a),len(b)
+        if l1>l2:b="0"*(l1-l2)+b
+        else:a="0"*(l2-l1)+a
+        str1,forward="",'0'
+        for i in range(max(l1,l2)-1,-1,-1):
+            c=collections.Counter([a[i],b[i],forward])
+            if c['1']>=2:
+                forward='1'
+                str1=str(c['1']-2)+str1
+            else:
+                forward='0'
+                str1=str(c['1'])+str1
+        else:
+            if forward=='1':
+                str1='1'+str1
+        return str1
+
+import collections
+class Solution:
+    def addBinary(self, a,b):
+        l1,l2=len(a),len(b)
+        if l1>l2:b="0"*(l1-l2)+b
+        else:a="0"*(l2-l1)+a
+        forward='0'
+        for i in range(max(l1,l2)-1,-1,-1):
+            c=collections.Counter([a[i],b[i],forward])
+            if c['1']>=2:
+                forward='1'
+                a[i]=str(c['1']-2)
+            else:
+                forward='0'
+                a[i]=str(c['1'])+str1
+        else:
+            if forward=='1':
+                a='1'+a
+        return a
 
 if __name__=="__main__":
 	s=Solution()
