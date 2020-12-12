603ccddcbfba6eeb21f5db35f5bae2ec6cd9f144
tongjirc <44321546+tongjirc@users.noreply.github.com>
2020-12-12 11:27:51 +0800

    chore: first exercise for cpp

--- test/CMakeProject1/includes/CMakeProject1.h
+++ test/CMakeProject1/includes/CMakeProject1.h
@@ -3,6 +3,25 @@
 
 #pragma once
 
+#define _CRTDBG_MAP_ALLOC
+
+#ifdef _MSC_VER
+#include <crtdbg.h>
+
+#else
+#define _ASSERT(expr) ((void)0)
+
+#define _ASSERTE(expr) ((void)0)
+#endif
+
+
+#include <stdlib.h>
+#include <unordered_map>
+#include <hash_map>
+#include <hash_set>
+#include <map>
+#include <memory>
 #include <iostream>
+#include <vector>
 
 // TODO: 在此处引用程序需要的其他标头。
